0ef62714d58f8918f8f0134033079906c59c8252
Raihanafif28 <mahendrasuog@gmail.com>
2023-07-23 19:17:09 +0700

    main

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,67 +1,63 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 
+// Register a new user
 const register = async (req, res) => {
     try {
-        const { username, password, role } = req.body;
-
-        // Check if the username already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Username already exists' });
-        }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
-        const newUser = new User({
-            username,
-            password: hashedPassword,
-            role: role || 'user',
-        });
-
-        await newUser.save();
-
-        res.status(201).json({ message: 'User registered successfully' });
+      const { username, password } = req.body;
+  
+      // Check if username already exists
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Username already exists' });
+      }
+  
+      // Create a new user
+      const newUser = new User({
+        username,
+        password, // Store the password as plain text
+      });
+  
+      // Save the user to the database
+      await newUser.save();
+  
+      res.json({ message: 'User registered successfully' });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal server error' });
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
     }
-};
-
-const login = async (req, res) => {
+  };
+  
+  // Login logic
+  const login = async (req, res) => {
     try {
-        const { username, password } = req.body;
-
-        // Check if the username exists
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        // Check if the password is correct
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        // Generate JWT token
-        const token = jwt.sign({ user: user.username }, 'raihan', { expiresIn: '1h' });
-
-        // Set the JWT token as a cookie
-        res.cookie('token', token, { maxAge: 3600000, httpOnly: true }); // Expiry set to 1 hour (3600000 milliseconds)
-        const role = user.role;
-
-
-        res.json({ token, role });              
+      const { username, password } = req.body;
+  
+      // Check if user exists
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+  
+      // Check password
+      if (user.password !== password) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+  
+      // Generate JWT token
+      const token = jwt.sign({ user: user.username }, 'raihan', { expiresIn: '1h' });
+  
+      // Set the JWT token as a cookie
+      res.cookie('token', token, { maxAge: 3600000, httpOnly: true }); // Expiry set to 1 hour (3600000 milliseconds)
+  
+      // Return the token as a response
+      res.json({ token });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal server error' });
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
     }
-};
+  };
 
 module.exports = { register, login };
