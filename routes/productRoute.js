3822b37f896c9c18e4222f0063294c32df1af736
Raihanafif28 <mahendrasuog@gmail.com>
2023-07-23 18:05:12 +0700

    main

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
+const isAuthenticated = require('../middleware/authMiddleware')
 
 // Rute untuk mendapatkan daftar produk
-router.get('/products', async (req, res) => {
+router.get('/products',isAuthenticated, async (req, res) => {
     try {
         const products = await Product.find();
         res.json(products);
@@ -14,7 +15,7 @@ router.get('/products', async (req, res) => {
 });
 
 // Rute untuk mendapatkan detail produk berdasarkan ID
-router.get('/products/:id', async (req, res) => { 
+router.get('/products/:id',isAuthenticated, async (req, res) => { 
     try {
         const productId = req.params.id;
         console.log(productId);
@@ -45,7 +46,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Add the Multer middleware to your routes
-router.post('/products', upload.single('image'), async (req, res) => {
+router.post('/products',isAuthenticated, upload.single('image'), async (req, res) => {
     try {
         const newProduct = req.body;
         console.log(newProduct);
@@ -60,7 +61,7 @@ router.post('/products', upload.single('image'), async (req, res) => {
 
 
 // Rute untuk memperbarui produk berdasarkan ID
-router.put('/products/:id', async (req, res) => {
+router.put('/products/:id',isAuthenticated, async (req, res) => {
     try {
         const productId = req.params.id;
         console.log(productId);
@@ -77,7 +78,7 @@ router.put('/products/:id', async (req, res) => {
 });
 
 // Rute untuk menghapus produk berdasarkan ID
-router.delete('/products/:id', async (req, res) => {
+router.delete('/products/:id', isAuthenticated,async (req, res) => {
     try {
         const productId = req.params.id;
         console.log(productId);
@@ -93,7 +94,7 @@ router.delete('/products/:id', async (req, res) => {
 });
 
 // Pembelian produk
-router.post('/products/:id/buy', async (req, res) => {
+router.post('/products/:id/buy',isAuthenticated, async (req, res) => {
     const quantity = req.body.quantity;
 
     if (isNaN(quantity) || quantity <= 0) {
