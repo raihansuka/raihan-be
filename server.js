5873ee37c4644fad5e5667c25cf962607b81d890
Raihanafif28 <mahendrasuog@gmail.com>
2023-07-24 18:00:47 +0700

    main

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 require('./datauye');
 
 // Middleware
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://raihan-fe.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(bodyParser.json());
 app.use('/img', express.static(path.join(__dirname, 'img')))
 
